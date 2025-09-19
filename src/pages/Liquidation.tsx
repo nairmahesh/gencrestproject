189cf628cb8c14c3967aea75ca2c067d2c03037e
Mahesh Nair <maheshnair73@gmail.com>
2025-09-19 11:40:09 +0530

    Fix JSX comment syntax error in Liquidation.tsx

--- src/pages/Liquidation.tsx
+++ src/pages/Liquidation.tsx
@@ -622,7 +622,6 @@ const Liquidation: React.FC = () => {
         </div>
       </div>
 
-      {/* Stock Verification Modal with SKU Color Tags */}
       {showVerifyModal && selectedDistributor && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-xl w-full max-w-4xl max-h-[90vh] overflow-hidden">
